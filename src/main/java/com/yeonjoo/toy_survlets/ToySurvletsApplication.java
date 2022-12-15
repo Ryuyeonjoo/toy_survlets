82ad2cb6fa9b81e8d39918df31027a6b04b12c62
Ryuyeonjoo <fuswn87@gmail.com>
2022-12-15 09:30:17 +0900

    first commit

---
+++ src/main/java/com/yeonjoo/toy_survlets/ToySurvletsApplication.java
@@ -0,0 +1,13 @@
+package com.yeonjoo.toy_survlets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToySurvletsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToySurvletsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yeonjoo/toy_survlets/ToySurvletsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeonjoo.toy_survlets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToySurvletsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
